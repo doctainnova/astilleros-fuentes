be828347b45f0e32c46c2e56a1dedd49ed0bbed8
fran sayago <fransayago04@gmail.com>
2026-03-24 20:23:00 -0300

    feature: push de v2, pagina web optimizada

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
+  compressHTML: true,
+
+  build: {
+    inlineStylesheets: 'auto',
   },
+
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+      minify: 'esbuild',
+    },
+  },
+
   image: {
     service: sharpImageService({
       limitInputPixels: false,
